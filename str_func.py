d0df5d550d4a2594de050e82c1abe0c9a7d339d6
Viktor Nikulaev <viktor_1611@mail.ru>
2024-05-20 00:11:11 +0300

    add funcs capitalize

--- str_func.py
+++ str_func.py
@@ -2,3 +2,10 @@ def upper():
     """Функция upper"""
     a = 'bcdefg'
     print(a.upper())
+
+
+def capitalize():
+    """функция capitalize"""
+    proposal = "everyone sees the world in one's own way"
+    print(proposal.capitalize())
+
